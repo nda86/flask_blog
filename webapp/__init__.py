442e14a25aeef0af815334dcd3c79c47c5b7a603
nda86 <nda86@mail.ru>
2020-08-27 01:14:50 +0800

    dd comment to create_app

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -4,8 +4,19 @@ from flask import Flask
 
 
 def create_app(config=None):
+	"""
+	фабрика для создания инстанса сервера flask
+	:param config: полный путь к классу конфигурации(example: app_config.DevConfig or app_config.ProdConfig)
+	в виде строки или ссылка на объект класса конфигурации, предварительно его нужно импортировать
+	:return: app - инстанс класса Flask - экземпляр сервера
+	"""
+
 	app = Flask(__name__)
+
+	# если имя или путь к классу конфигурации не передан, то загружаем файл конфигурации в зависимости
+	# от переменной окружения ENV, её дефолтное значение устанавливаем в 'dev'
 	config = config or f"app_config.{os.environ.get('ENVIROMENT', 'dev').capitalize()}Config"
 	app.config.from_object(config)
 
 	return app
+
