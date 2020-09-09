6be9f1b7a3839d56af2c74cfecc81536df49ecfe
nda86 <nda86@mail.ru>
2020-09-09 23:16:47 +0800

    add macro paginate

--- sql_tester.py
+++ sql_tester.py
@@ -5,5 +5,6 @@ app = create_app()
 
 with app.app_context():
 	posts = Post.query.order_by(Post.created_at.desc()).all()
-	for post in posts:
-		print(post.created_at)
+	print(len(posts))
+	pag = Post.query.order_by(Post.created_at.desc()).paginate(3, 5)
+	print(pag.prev())
--- webapp/blog/controllers.py
+++ webapp/blog/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 from flask_login import login_required, current_user
 
 from webapp.strings import Title
@@ -9,14 +9,17 @@ from webapp import get_logger
 blog_blueprint = Blueprint('blog', __name__, url_prefix='/blog', template_folder='../templates/blog')
 logger = get_logger("blog")
 
+
 @blog_blueprint.route('/')
 def home():
 	return render_template("blog_home.html", title='Home | Blog')
 
 
 @blog_blueprint.route('/posts', methods=['GET', 'POST'])
+@blog_blueprint.route('/posts/<int:page>', methods=['GET', 'POST'])
 @login_required
-def list_posts():
+def list_posts(page=None):
 	logger.debug(f"User {current_user.username} has access to the blog page")
-	posts = Post.query.order_by(Post.created_at.desc()).all()
+	page = page or 1
+	posts = Post.query.order_by(Post.created_at.desc()).paginate(page=page, per_page=current_app.config.get("POST_PER_PAGE", 5))
 	return render_template("blog_posts.html", title=Title.tBlogPosts, posts=posts)
